d0590c43acf5d306379e6d48a6c44a133850e6f4
isoscanning <isoscanning@gmail.com>
2025-11-26 00:16:43 -0300

    fix: config prod build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -8,6 +14,13 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': resolve(__dirname, '.'),
+    };
+    return config;
   },
 }
 

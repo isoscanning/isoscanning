64c06a8679c18b10a3f08860757564174a4465df
isoscanning <isoscanning@gmail.com>
2026-06-19 13:34:35 -0300

    Ajuste pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export const metadata: Metadata = {
 };
 
 import { GoogleAnalytics } from '@next/third-parties/google'
-import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -65,6 +64,28 @@ export default function RootLayout({
         <meta name="application-name" content="ISO Scanning" />
         <meta name="msapplication-TileColor" content="#0a0a0a" />
         <meta name="theme-color" content="#0a0a0a" />
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1583034490491485');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1583034490491485&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className="min-h-screen bg-background text-foreground">
         <ThemeProvider
@@ -79,24 +100,6 @@ export default function RootLayout({
           </AuthProvider>
         </ThemeProvider>
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ""} />
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1583034490491485');
-fbq('track', 'PageView');`}
-        </Script>
-        <noscript>
-          <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1583034490491485&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
       </body>
     </html>
   );
